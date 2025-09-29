8e9ac914558237de798f8e394061a6764afd421c
Taylor Rhyne <tcrhyne@gmail.com>
2025-09-29 15:20:13 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = (env, argv) => ({
       // Converts TypeScript code to JavaScript
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
 
-      // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, use: ['style-loader', { loader: 'css-loader' }] },
+      // Simple CSS handling without PostCSS
+      { test: /\.css$/, use: ['style-loader', { loader: 'css-loader', options: { importLoaders: 1, modules: false } }] },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
       { test: /\.(png|jpg|gif|webp|svg)$/, loader: 'url-loader' },
