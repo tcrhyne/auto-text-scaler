8ea49cd7731648b37c5fa8369ea96b0208f69f63
Taylor Rhyne <tcrhyne@gmail.com>
2025-09-29 15:23:27 -0500

    remove full page updates; support min and max size constraints

--- src/code.ts
+++ src/code.ts
@@ -1,25 +1,19 @@
 //This plugin resizes text to fit in its textbox
 
 // show form
-figma.showUI(__html__, { width: 400, height: 290 });
+figma.showUI(__html__, { width: 400, height: 380 });
 
 figma.ui.onmessage = msg => {
-  if (msg.type === 'page') {
-    const nodes: SceneNode[] = figma.currentPage.findAll(node => node.type === "TEXT")
-    nodes.forEach((node =>{
-      if (node.type === 'TEXT'){
-        loadFont(node.fontName).then(
-            ()=>resizeText(node)
-          )
-      }
-    }))
-  }
+  // Extract font size constraints if provided
+  const minFontSize = msg.minFontSize !== null ? msg.minFontSize : undefined;
+  const maxFontSize = msg.maxFontSize !== null ? msg.maxFontSize : undefined;
+  
   if (msg.type === 'selection'){
     const nodes = figma.currentPage.selection
     nodes.forEach((node =>{
       if (node.type === 'TEXT'){
         loadFont(node.fontName).then(
-            ()=>resizeText(node)
+            ()=>resizeText(node, minFontSize, maxFontSize)
           )
       }
     }))
@@ -31,14 +25,15 @@ figma.ui.onmessage = msg => {
     nodes.forEach((node =>{
       if (node.type === 'TEXT'){
         loadFont(node.fontName).then(
-            ()=>resizeText(node)
+            ()=>resizeText(node, minFontSize, maxFontSize)
           )
       }
     }))
   }
-
-  // Need to load fonts before closing plugin 
-  // figma.closePlugin()
+  
+  if (msg.type === 'cancel') {
+    figma.closePlugin()
+  }
 }
 
 
@@ -49,43 +44,56 @@ async function loadFont(fontName){
 
 
 
-function resizeText(node: TextNode){
+function resizeText(node: TextNode, minFontSize?: number, maxFontSize?: number){
   const _height = node.height
   node.textAutoResize = "HEIGHT"
   var _currentHeight = node.height
   if (_currentHeight > _height){
-    shrinkText(node, _height)
+    shrinkText(node, _height, minFontSize)
   }
   else{
-    growText(node, _height)
+    growText(node, _height, maxFontSize)
   }
   node.textAutoResize = "NONE"
   node.resize(node.width, _height)
 }
 
-function shrinkText(node: TextNode,  desiredHeight: number){
+function shrinkText(node: TextNode, desiredHeight: number, minFontSize?: number){
   var currentHeight = node.height
 
   while (currentHeight > desiredHeight){
     if (typeof(node.fontSize) === "number"){
-      node.fontSize -= 1
+      // Check if we've reached the minimum font size
+      if (minFontSize !== undefined && node.fontSize <= minFontSize) {
+        break;
+      }
+      // Use 0.5pt increments instead of 1.0pt
+      node.fontSize -= 0.5
     }
     currentHeight = node.height
-
   }
 }
 
-function growText(node: TextNode, desiredHeight: number){
+function growText(node: TextNode, desiredHeight: number, maxFontSize?: number){
   var currentHeight = node.height
+  var hitMaxSize = false
 
   while (currentHeight < desiredHeight){
     if (typeof(node.fontSize) === "number"){
-      node.fontSize += 1
+      // Check if we've reached the maximum font size
+      if (maxFontSize !== undefined && node.fontSize >= maxFontSize) {
+        hitMaxSize = true;
+        break;
+      }
+      // Use 0.5pt increments instead of 1.0pt
+      node.fontSize += 0.5
     }
     currentHeight = node.height
   }
-  // fontsize is now one size bigger than limit
-  if (typeof(node.fontSize) === "number"){
-    node.fontSize -= 1
+  
+  // Only decrease font size if we didn't hit the max size constraint
+  // This allows text to go exactly up to the max size if specified
+  if (!hitMaxSize && typeof(node.fontSize) === "number"){
+    node.fontSize -= 0.5
   }
 }
